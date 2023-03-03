0e4774e339d0a589627409d5a31dfee14d73fdbb
PanagiotopoulosK <pan.konstantinos@gmail.com>
2023-03-03 18:48:23 +0100

    update the code

--- MEvA-X_V1.0.0.py
+++ MEvA-X_V1.0.0.py
@@ -1763,15 +1763,15 @@ def picklefy_variables(var,var_name,output_folder):
 if __name__ == "__main__":
     prog_time = time.time()
 
-    dataset_filename=sys.argv[1]
-    labels_filename=sys.argv[2]
-    population=int(sys.argv[3])
-    generations=int(sys.argv[4])
-    two_points_crossover_probability=float(sys.argv[5])
-    arithmetic_crossover_probability=float(sys.argv[6])
-    mutation_probability=float(sys.argv[7])	
-    goal_significances_filename=sys.argv[8]
-    num_of_folds=int(sys.argv[9])
+    #dataset_filename=sys.argv[1]
+    #labels_filename=sys.argv[2]
+    #population=int(sys.argv[3])
+    #generations=int(sys.argv[4])
+    #two_points_crossover_probability=float(sys.argv[5])
+    #arithmetic_crossover_probability=float(sys.argv[6])
+    #mutation_probability=float(sys.argv[7])	
+    #goal_significances_filename=sys.argv[8]
+    #num_of_folds=int(sys.argv[9])
     
     #####
     abspath = os.path.abspath(__file__)
@@ -1779,23 +1779,10 @@ if __name__ == "__main__":
     os.chdir(dname)
     #####
     current_dir = os.getcwd()
-    FS_dir = os.path.join(current_dir, 'diet/FS_methods/')
-    #dataset_filename = './diet/diet_dataset_batch_effect_removed.txt'
-    #labels_filename = './diet/diet_labels.txt'
-    # ans = input("Running from Google Drive? (Y/N): ")
-    # if ans.upper() in ['Y','YES']:
-        # dataset_filename = '/content/drive/MyDrive/Colab Notebooks/diet_dataset_batch_effect_removed.txt'#/gdrive/My Drive/Colab Notebooks/
-        # labels_filename = '/content/drive/MyDrive/Colab Notebooks/diet_labels.txt'
-    # else:
-        # ans = input("Running locally? (Y/N): ")
-        # if ans.upper() in ['Y','YES']:
-            # dataset_filename = './diet/diet_dataset.txt'
-            # labels_filename = './diet/diet_labels.txt'
-    # # 			dataset_filename = 'C:/Users/kosta/Desktop/Thesis/My_code/diet/diet_dataset.txt'
-    # # 			labels_filename = 'C:/Users/kosta/Desktop/Thesis/My_code/diet/diet_labels.txt'
-        # else:
-            # dataset_filename = os.path.join(os.path.expanduser('~'), 'My_code', 'diet', 'diet_dataset.txt')
-            # labels_filename = os.path.join(os.path.expanduser('~'), 'My_code', 'diet', 'diet_labels.txt')
+    FS_dir = os.path.join(current_dir, 'Data/Ornish/FS_methods/')
+    dataset_filename = './Data/Ornish/diet_dataset_batch_effect_removed.txt'
+    labels_filename = './Data/Ornish/diet_labels.txt'
+
 
     [dataset, feature_names, sample_names, labels] = preprocessing_function(dataset_filename,labels_filename, as_pandas=True)
 
@@ -1805,7 +1792,10 @@ if __name__ == "__main__":
     max_values = np.array([5,	3,	11,	101, 0.35,	7,	10,	10,	8,	15,	5])#, 1.0, 1.0]) # 8.lambda  9.alpha  10.min_child_weight  11.scale_pos_weight  12.colsample  13.subsample
     parameters = max_values.shape[0]
 
-    num_of_folds = int(input("K-fold = "))
+    population = int(input("Population = "))
+	generations = int(input("Generations = "))
+	num_of_folds = int(input("K-fold = "))
+
 
     '''This section is here to test the Feature selection methods'''
     FS_calc = input("Do you need to calculate the feature selection algorithms? (Y/N): ")
@@ -1824,26 +1814,22 @@ if __name__ == "__main__":
         FS_calc = False
     '''up to this line.'''
 
-    #two_points_crossover_probability = 0.9
-    #arithmetic_crossover_probability = 0
-    #mutation_probability = 0.05
+    two_points_crossover_probability = 0.9
+    arithmetic_crossover_probability = 0
+    mutation_probability = 0.05
     # G1:Feature_compl, G2:Acc, G3:Split_compl, G4:wGM, G5:F1, G6:F2, G7:Precision, G8:Recall, G9:AUC, G10:Manhatan_dist, G11:GM
     ans = str(input('Use the unweighted (default) metrics? (Y/N): '))
     if ans.upper() in ['Y','YES']:
         goal_significances = np.ones(11)
     else:
+    	# Give the weights for the evaluation metrics here
         goal_significances = np.array([0.8,0.8,0.8,2,1,1,1,1,2,0.5,2])
 
     print(f'Weights: {goal_significances}')
 
     tstamp = time.strftime('%Y_%m_%d')
     output_folder = os.path.join(dname,'XGB_Results_diet\\'+f'P{population}_G{generations}_K{num_of_folds}'+'_'+str(time.time())+'\\')
-    # 	if dataset_filename == 'C:/Users/kosta/Desktop/Thesis/My_code/diet/diet_dataset.txt':
-    # 		output_folder = 'C:/Users/kosta/Desktop/Thesis/My_code/Results_directory/'+str(tstamp)+'_'+str(time.time())+'/'
-    # 	elif dataset_filename == os.path.join(os.path.expanduser('~'), 'My_code', 'diet', 'diet_dataset.txt'):
-    # 		output_folder = os.path.join(os.path.expanduser('~'), 'My_code', 'Results_directory',f'{str(tstamp)}_{str(time.time())}/')
-    # 	else:
-    # 		output_folder = '/content/drive/MyDrive/Colab Notebooks/Results_directory/'+str(tstamp)+'_'+str(time.time())+'/'
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
